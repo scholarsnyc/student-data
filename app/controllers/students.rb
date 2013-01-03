f8be4c8f231fa899f395c4daaced2fe3a143bb14
Steve Kinney <hello@stevekinney.net>
2013-01-03 09:38:22 -0500

    Add in controller for filtering students eligible for National Honor Society

--- app/controllers/students.rb
+++ app/controllers/students.rb
@@ -40,6 +40,12 @@ StudentDatabase.controllers :students do
     @students = Student.ineligible.all(order: [ :homeroom.asc ]).all(params)
     render 'students/index'
   end
+
+  get :honors do
+    @title = "Students Eligible for the National Honor Society"
+    @students = Student.all(grade: [7,10], order: [ :grade.asc  ]).keep_if {|s| s.records(:score.lt => 90).count == 0}
+    render 'students/index'
+  end
   
   get :probation, :map => "/students/probation/:level" do
     @title = "Probation: Level #{params[:level]}"
