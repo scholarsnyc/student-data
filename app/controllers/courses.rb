bbd92d2bd35301d133f67f3f72f5eae85dafe0b4
Steve Kinney <hello@stevekinney.net>
2013-02-04 11:31:46 -0500

    Remove course weights

--- app/controllers/courses.rb
+++ app/controllers/courses.rb
@@ -14,16 +14,5 @@ StudentDatabase.controllers :courses do
     @records = @course.records(mp: @marking_period, year: @year)
     render 'courses/show'
   end
-
-  get :weights do
-    @courses = Course.all(params).all(order: [ :code.asc ])
-    render 'courses/weights'
-  end
-  
-  post :weight, map: "/courses/:course/weight/:weight", provides: :json  do 
-    @course = Course.get(params[:course])
-    @course.update(weight: params[:weight])
-    render json: {course: @course, success: true}
-  end
   
 end
