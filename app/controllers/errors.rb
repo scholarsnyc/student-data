74b4f865911a24afcef64da1178231b3cfce72e2
Steve Kinney <hello@stevekinney.net>
2013-02-07 09:51:00 -0500

    Load in new data files

--- app/controllers/errors.rb
+++ app/controllers/errors.rb
@@ -1,11 +1,7 @@
 StudentDatabase.controllers :errors do
   
   not_found do
-    "These are not the droids you are looking for."
+    "This page does not exist."
   end
 
-  error 500 do
-    "Steve screwed something up."
-  end
-  
 end
