95fe9717cd0377dea222aff7e15a60be989a461f
Steve Kinney <hello@stevekinney.net>
2013-02-08 12:35:13 -0500

    Add tablesorter defaults for courses and students on probation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
   $("table#student-index").tablesorter( {sortList: [[1,0]] } ); 
+  $("table#course-index").tablesorter( {sortList: [[0,0]] } ); 
+  $("table#probation-index").tablesorter( {sortList: [[1,0]] } ); 
 });
