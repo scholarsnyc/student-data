0733790a60c3a2a49f89ede227cd8b659d98aa66
Steve Kinney <hello@stevekinney.net>
2013-02-08 13:01:08 -0500

    Get the grade filter working on the client-side

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
+
+  // Tablesorter Defaults
   $("table#student-index").tablesorter( {sortList: [[1,0]] } ); 
   $("table#course-index").tablesorter( {sortList: [[0,0]] } ); 
   $("table#probation-index").tablesorter( {sortList: [[1,0]] } ); 
+
+  var studentRows = $('table#student-index > tbody tr'),
+      filterButtons = $('div#grade-filter button');
+
+  filterButtons.on('click', function (e) {
+    var activeRows, inactiveRows, grade = $(this).data("grade");
+    e.preventDefault();
+    if (grade === "all") {
+      studentRows.show
+    } else {
+      activeRows = studentRows.filter(function () {
+        return $(this).data("grade") === grade;
+      });
+      inactiveRows = studentRows.filter(function () {
+        return $(this).data("grade") !== grade;
+      });
+      activeRows.show();
+      inactiveRows.hide();
+    }
+  });
 });
