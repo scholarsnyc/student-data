48adba1115b4d72ebdbe0fbd409a56e52ccffe93
Steve Kinney <hello@stevekinney.net>
2013-02-12 10:11:07 -0500

    Separate out first and last names

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $("table#student-index").tablesorter( {sortList: [[1,0]] } ); 
   $("table#course-index").tablesorter( {sortList: [[0,0]] } ); 
   $("table#probation-index").tablesorter( {sortList: [[1,0]] } ); 
-  $("table#course-show").tablesorter( {sortList: [[0,0]] } ); 
+  $("table#course-show").tablesorter( {sortList: [[1,0]] } ); 
 
 
   var studentRows = $('table#student-index > tbody tr'),
