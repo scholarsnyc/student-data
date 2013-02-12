d3024b64c0c752e6edd3fced2ecfe3b1e87ab096
Steve Kinney <hello@stevekinney.net>
2013-02-12 10:09:12 -0500

    Add table sorter to Course view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
   $("table#student-index").tablesorter( {sortList: [[1,0]] } ); 
   $("table#course-index").tablesorter( {sortList: [[0,0]] } ); 
   $("table#probation-index").tablesorter( {sortList: [[1,0]] } ); 
+  $("table#course-show").tablesorter( {sortList: [[0,0]] } ); 
+
 
   var studentRows = $('table#student-index > tbody tr'),
       filterButtons = $('div#grade-filter button');
